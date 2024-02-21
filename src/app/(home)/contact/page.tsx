c7ce26e172716f3dbc8fe9297cf236d645c382a3
erxiao <er735763@dal.ca>
2024-02-20 21:16:41 -0400

    Contact Page update

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -1,7 +1,35 @@
 import AppBar from '@/components/AppBar';
 import Box from '@mui/material/Box';
+import ContactCard from '@/components/contact/ContactCard';
+import Typography from '@mui/material/Typography';
 
 export default function ContactPage() {
+	const Contacts = [
+		{
+			name: 'Ahmed Galal',
+			email: 'a.galal@dal.ca'
+		},
+		{
+			name: 'Dave Chuck',
+			email: 'dchuck@dal.ca'
+		},
+		{
+			name: 'Erxiao Tang',
+			email: 'erxiao.tang@dal.ca'
+		},
+		{
+			name: 'Kevin Orenday',
+			email: 'kevin.orenday@dal.ca'
+		},
+		{
+			name: 'Maximo Guk',
+			email: 'maximo@dal.ca'
+		},
+		{
+			name: 'Saahir Monowar',
+			email: 'saahir.monowar@dal.ca'
+		},
+	];
 	return (
 		<>
 			<AppBar />
@@ -9,11 +37,18 @@ export default function ContactPage() {
 				<Box
 					sx={{
 						display: 'flex',
+						flexDirection: 'column',
 						justifyContent: 'center',
 						alignItems: 'center',
+						gap: 2,
 					}}
 				>
-					<div>Contact us!</div>
+					<Typography variant="h3" component="div" sx={{marginTop: 2}}>
+							Contact us!
+						</Typography>
+					{Contacts.map((contact, index) => (
+                        <ContactCard key={index} contact={contact} />
+                    ))}
 				</Box>
 			</main>
 		</>
