43881872ed0df68bfb109dad8249adfc2cf11ed6
= <sh843173@dal.ca>
2024-03-27 14:10:06 -0300

    begin designing page slightly

--- src/components/route-history/RouteHistoryArea.tsx
+++ src/components/route-history/RouteHistoryArea.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import type { IRouteHistoryEntry } from '@/models/schemas/RouteHistoryEntry';
+import { Box, Button, Container, Typography } from '@mui/material';
 import { useState } from 'react';
 
 export default function RouteHistoryArea({
@@ -10,6 +11,18 @@ export default function RouteHistoryArea({
 }) {
 	const [routeHistory, setRouteHistory] =
 		useState<IRouteHistoryEntry[]>(initialRouteHistory);
+	const [filteredRouteHistory, setFilteredRouteHistory] =
+		useState(routeHistory);
 
-	return <>{JSON.stringify(routeHistory)}</>;
+	return (
+		<Container maxWidth="sm" sx={{ p: 1 }}>
+			<Box display="flex" justifyContent="space-between" alignItems="center">
+				<Typography variant="h3" component="h2">
+					Route History
+				</Typography>
+				<Button variant="contained">Filter</Button>
+			</Box>
+			{JSON.stringify(filteredRouteHistory)}
+		</Container>
+	);
 }
