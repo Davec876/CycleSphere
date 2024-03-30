9df235725a680d09ed8059c179f4af96824a78f7
= <sh843173@dal.ca>
2024-03-29 23:56:38 -0300

    fetch route names higher up in component tree

--- src/components/route-history/RouteHistoryPage.tsx
+++ src/components/route-history/RouteHistoryPage.tsx
@@ -1,18 +1,28 @@
+import { getRoute } from '@/service/Route';
 import { getRouteHistory } from '@/service/User';
 import { APIError } from '@/util/errors/APIError';
 import { redirect } from 'next/navigation';
+import { cache } from 'react';
+import type { CompleteRHE } from './RouteHistoryArea';
 import RouteHistoryArea from './RouteHistoryArea';
-import type { IRouteHistoryEntry } from '@/models/schemas/RouteHistoryEntry';
+
+const getRouteCached = cache(getRoute);
 
 export default async function RouteHistoryPage() {
-	const routeHistory: IRouteHistoryEntry[] = await getRouteHistory().catch(
-		(e) => {
+	const routeHistory: CompleteRHE[] = await getRouteHistory()
+		.then(async (RHEs) => {
+			const routePromises = RHEs.map(async (RHE) =>
+				getRouteCached(RHE.routeId)
+			);
+			const routes = await Promise.all(routePromises);
+			return RHEs.map((RHE, i) => ({ ...RHE, route: routes[i] }));
+		})
+		.catch((e) => {
 			if (e instanceof APIError && e.message === 'User is not logged in!') {
 				redirect('/');
 			}
 			throw e;
-		}
-	);
+		});
 
 	return <RouteHistoryArea routeHistory={routeHistory} />;
 }
