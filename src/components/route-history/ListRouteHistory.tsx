a4ae0891bb86b31914e7b34d0fd12606be291487
= <sh843173@dal.ca>
2024-03-29 23:05:31 -0300

    Extract RHECard, tweak invalidRoute logic

--- src/components/route-history/ListRouteHistory.tsx
+++ src/components/route-history/ListRouteHistory.tsx
@@ -1,17 +1,7 @@
 import type { IRouteHistoryEntry } from '@/models/schemas/RouteHistoryEntry';
-import { getRoute } from '@/service/Route';
-import {
-	Box,
-	Card,
-	CardContent,
-	CardHeader,
-	Link,
-	Skeleton,
-	Typography,
-} from '@mui/material';
+import { Card, CardContent, CardHeader } from '@mui/material';
 import { DateTime } from 'luxon';
-import NextLink from 'next/link';
-import { cache, useEffect, useState } from 'react';
+import RHECard from './RHECard';
 
 export default function ListRouteHistory({
 	routeHistory,
@@ -75,56 +65,5 @@ function RHGroupCard({ rhGroup }: { rhGroup: RHGroup }) {
 				))}
 			</CardContent>
 		</Card>
-	);
-}
-
-function RHECard({ RHE }: { RHE: IRouteHistoryEntry }) {
-	const [routeName, setRouteName] = useState<string | null>(null);
-	const [invalidRoute, setInvalidRoute] = useState(false);
-	const datetime = DateTime.fromISO(RHE.datetimeISO);
-
-	useEffect(() => {
-		const getRouteCached = cache(getRoute);
-		getRouteCached(RHE.routeId).then((route) => {
-			if (!route) {
-				setInvalidRoute(true);
-				setRouteName('Unknown Route');
-			} else {
-				setRouteName(route.title);
-			}
-		});
-	}, [RHE]);
-
-	return (
-		<Link
-			href={`/routes/${RHE.routeId}`}
-			component={NextLink}
-			underline="none"
-			color="unset"
-		>
-			<Box
-				sx={{
-					':hover': { bgcolor: (theme) => theme.palette.action.hover },
-				}}
-				border={(theme) => `1px solid ${theme.palette.text.primary}`}
-				p={1}
-				display="flex"
-				justifyContent="space-between"
-				alignItems="center"
-			>
-				<Typography
-					variant="h5"
-					component="h4"
-					color={invalidRoute ? 'red' : undefined}
-				>
-					{routeName ?? <Skeleton width={200} animation="wave" />}
-				</Typography>
-				<Typography textAlign="end" suppressHydrationWarning>
-					{datetime.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}
-					<br />
-					{datetime.toLocaleString(DateTime.TIME_SIMPLE)}
-				</Typography>
-			</Box>
-		</Link>
 	);
 }
