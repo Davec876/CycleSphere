ab4a805745ebb7b455db4cadc15351ce62768568
kevino29 <varax297@gmail.com>
2024-04-01 23:12:42 -0300

    moved list item from activity tab to a separate component

--- src/components/fitness/tabs/ActivitiesTab.tsx
+++ src/components/fitness/tabs/ActivitiesTab.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { getActivitiesByUserId } from '@/service/Activity';
-import { formatDate } from '@/util/formatDate';
 import type { IProfile } from '@/models/Profile';
 import type { IActivity } from '@/models/Activity';
 import BaseTabPanel from './BaseTabPanel';
@@ -11,12 +10,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Button from '@mui/material/Button';
-import CheckIcon from '@mui/icons-material/Check';
-import DeleteIcon from '@mui/icons-material/Delete';
+import ActivityListItem from '../ActivityListItem';
 
 export default function ActivitiesTab(props: {
 	index: number;
@@ -25,18 +19,19 @@ export default function ActivitiesTab(props: {
 }) {
 	const [filtered_list, setList] = useState([] as IActivity[]);
 	const [activities, setActivities] = useState(null as unknown as IActivity[]);
+	const [reload, setReload] = useState(false);
 
 	useEffect(() => {
-		if (!activities) {
-			getActivitiesByUserId(props.profile.id)
-				.then((activities: IActivity[]) => {
-					setList(activities);
-					setActivities(activities);
-				})
-				.catch((error) => console.log(error));
-		}
+		getActivitiesByUserId(props.profile.id)
+			.then((activities: IActivity[]) => {
+				setList(activities);
+				setActivities(activities);
+			})
+			.catch((error) => console.log(error));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [reload]);
+
+	const forceReload = () => setReload(!reload);
 
 	return (
 		<BaseTabPanel index={props.index} value={props.value}>
@@ -76,32 +71,11 @@ export default function ActivitiesTab(props: {
 			{filtered_list.length > 0 ? (
 				<List>
 					{filtered_list.map((activity, index) => (
-						<ListItem
+						<ActivityListItem
+							activity={activity}
 							key={`activity-item-${index}`}
-							secondaryAction={
-								<ButtonGroup variant="outlined">
-									<Button
-										variant="outlined"
-										color="success"
-										startIcon={<CheckIcon />}
-									>
-										Complete
-									</Button>
-									<Button
-										variant="outlined"
-										color="error"
-										startIcon={<DeleteIcon />}
-									>
-										Delete
-									</Button>
-								</ButtonGroup>
-							}
-						>
-							<ListItemText
-								primary={activity.name}
-								secondary={formatDate(activity.createdAt)}
-							/>
-						</ListItem>
+							reload={forceReload}
+						/>
 					))}
 				</List>
 			) : (
