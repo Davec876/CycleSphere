bcc9b4b0abd7c66e557ebbbe85eb035500e2fda7
Asher <erxiao.tang@dal.ca>
2024-04-03 22:34:43 -0300

    move map to component

--- src/app/(home)/map/page.tsx
+++ src/app/(home)/map/page.tsx
@@ -1,36 +1,36 @@
 'use client';
-import { useState } from 'react';
 import AppBar from '@/components/AppBar';
 import Box from '@mui/material/Box';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
-import { AutocompleteCustom } from '@/components/routes/map/AutoCompleteCustom';
-import { BicyclingLayer } from '@/components/routes/map/BicyclingLayer';
-import MapHandler from '@/components/routes/map/MapHandler';
-
-export const communityMapId = 'community-shared-map';
+import { useSession } from 'next-auth/react';
+import MapEditFeature from '@/components/routes/map/MapEditFeature';
+import { Suspense } from 'react';
+import CommunityMap from '@/components/routes/map/CommunityMap';
 
 export default function MapPage() {
-	const [selectedPlace, setSelectedPlace] =
-		useState<google.maps.places.PlaceResult | null>(null);
+	const { data: session } = useSession();
 
 	return (
 		<>
 			<AppBar />
 			<main>
-				<Box sx={{ m: 2, height: [350, 425, 500] }}>
-					<APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-						<AutocompleteCustom onPlaceSelect={setSelectedPlace} />
-						<Map
-							id={communityMapId}
-							defaultZoom={12}
-							defaultCenter={{
-								lat: 44.64951641616885,
-								lng: -63.58510266385201,
-							}}
-						></Map>
-						<BicyclingLayer id={communityMapId} />
-						<MapHandler place={selectedPlace} />
-					</APIProvider>
+				<CommunityMap />
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Box
+						marginTop={1}
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							gap: 1,
+						}}
+					>
+						<Suspense>{session?.user && <MapEditFeature />}</Suspense>
+					</Box>
 				</Box>
 			</main>
 		</>
---
+++ src/components/routes/map/CommunityMap.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { AutocompleteCustom } from '@/components/routes/map/AutoCompleteCustom';
+import { BicyclingLayer } from '@/components/routes/map/BicyclingLayer';
+import MapHandler from '@/components/routes/map/MapHandler';
+
+export const communityMapId = 'community-shared-map';
+
+export default function CommunityMap() {
+	const [selectedPlace, setSelectedPlace] =
+		useState<google.maps.places.PlaceResult | null>(null);
+	return (
+		<Box sx={{ m: 2, height: [350, 425, 500] }}>
+			<APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
+				<AutocompleteCustom onPlaceSelect={setSelectedPlace} />
+				<Map
+					id={communityMapId}
+					defaultZoom={12}
+					defaultCenter={{
+						lat: 44.64951641616885,
+						lng: -63.58510266385201,
+					}}
+				></Map>
+				<BicyclingLayer id={communityMapId} />
+				<MapHandler place={selectedPlace} />
+			</APIProvider>
+		</Box>
+	);
+}
